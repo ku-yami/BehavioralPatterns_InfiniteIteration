98b96501df208f66f7d9737a7a6533dd66ca4394
yamil <yamil@inbox.ru>
2022-07-21 06:09:49 +0300

    Initial commit

---
+++ src/com/company/Randoms.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true; //бесконечный итератор
+            }
+
+            @Override
+            public Integer next() {
+                //случайное число в диапазоне [min..max]
+                return new Random().nextInt(max - min + 1) + min;
+            }
+        };
+    }
+}
